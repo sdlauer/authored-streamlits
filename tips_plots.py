4b1f905b550b528fcf3ff917bcf37ea17e075c5d
Chris Chan <chris.chan@zybooks.com>
2022-09-03 07:46:36 -0500

    Add files via upload

--- tips_plots.py
+++ tips_plots.py
@@ -1,16 +1,16 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import seaborn as sns
+import plotly.express as px
 import matplotlib.pyplot as plt
 
-tips = sns.load_dataset('tips')
+tips = px.data.tips()
 
 def plots(x, y, hue, plot):
-    if plot=='Strip plot': sns.stripplot(x=x, y=y, hue=hue, data=tips)
-    if plot=='Box plot': sns.boxplot(x=x, y=y, hue=hue, data=tips)
-    if plot=='Swarm plot': sns.swarmplot(x=x, y=y, hue=hue, data=tips)
-    if plot=='Violin plot': sns.violinplot(x=x, y=y, hue=hue, data=tips)
+    if plot=='Strip plot': px.strip(tips, x=x, y=y)
+    # if plot=='Box plot': sns.boxplot(x=x, y=y, hue=hue, data=tips)
+    # if plot=='Swarm plot': sns.swarmplot(x=x, y=y, hue=hue, data=tips)
+    # if plot=='Violin plot': sns.violinplot(x=x, y=y, hue=hue, data=tips)
 
 st.title('Visualizing the tips dataset')
 
@@ -30,7 +30,7 @@ y = st.selectbox(
      'Select numerical variable',
      ('tip','total_bill'))
 
-if plots == 'Violin plot': plots(x,y,hue,'Strip plot')
-elif plots == 'Box plot': plots(x,y,hue,'Box plot')
-elif plots == 'Swarm plot': plots(x,y,hue,'Swarm plot')
-else: plots(x,y,hue,'Violin plot')
+if plots == 'Strip plot': plots(x,y,hue,'Strip plot')
+# elif plots == 'Box plot': plots(x,y,hue,'Box plot')
+# elif plots == 'Swarm plot': plots(x,y,hue,'Swarm plot')
+# else: plots(x,y,hue,'Violin plot')
