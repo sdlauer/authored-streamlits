3cf8ee04279a9ec23e2ba2f538e535040930611f
cjschan <cjschan@gmail.com>
2022-09-03 13:23:37 -0500

    Update mpg_regression.py

--- mpg_regression.py
+++ mpg_regression.py
@@ -34,25 +34,31 @@ output_feat = st.selectbox(
     ]
 )
 
-group = st.selectbox(
-    "Grouping",
-    [
-        None,
-        "cylinders",
-        "model_year",
-        "origin"
-    ]
-)
-
 reg_line = st.checkbox("Regression line")
 reg_eq = st.checkbox("Regression equation")
 corr_coef = st.checkbox("Correlation coefficient")
 
+def show_eq(data):
+    m, b = np.polyfit(data[0], data[1], 1)
+    m = round(m,3)
+    b = round(b,3)
+    eq = 'y =' + str(m) + 'x + ' + str(b)
+    return eq
+
+def show_corr(data):
+    # Note: np.corrcoef gives a correlation matrix
+    corr_coef = np.corrcoef(data[0],data[1])
+    corr_coef = round(corr_coef[0,1],3)
+    plt.text(110,2,'r = ' + str(corr_coef))
+
 fig, ax = plt.subplots()
 
-# if group==None:
-#     ax = sns.regplot(x=input_feat, y=output_feat,
-#         data=mpg, fit_reg=reg_line, ci=None, line_kws={"color": "grey"})
-# elif group!=None:
-sns.lmplot(x='horsepower', y='mpg', hue='cylinders', data=mpg)
+ax = sns.regplot(x=input_feat, y=output_feat,
+    data=mpg, fit_reg=reg_line, ci=None, line_kws={"color": "grey"})
+
+if reg_eq: 
+    st.text(show_eq([mpg[input_feat],mpg[output_feat]]))
+
+if corr_coef:
+    st.text(show_corr([mpg[input_feat],mpg[output_feat]]))
 st.pyplot(fig)
