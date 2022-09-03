daec1fffff88479362ad4e0e3b983ec451e35bcb
Chris Chan <chris.chan@zybooks.com>
2022-09-03 07:35:15 -0500

    Add files via upload

--- tips_plots.py
+++ tips_plots.py
@@ -1,17 +1,18 @@
 import streamlit as st
-# import pandas as pd
-# import numpy as np
-# import seaborn as sns
-# import matplotlib.pyplot as plt
-#
-# tips = sns.load_dataset('tips')
-#
-# def plots(x, y, hue, plot):
-#     if plot=='Strip plot': sns.stripplot(x=x, y=y, hue=hue, data=tips)
-#     if plot=='Box plot': sns.boxplot(x=x, y=y, hue=hue, data=tips)
-#     if plot=='Swarm plot': sns.swarmplot(x=x, y=y, hue=hue, data=tips)
-#     if plot=='Violin plot': sns.violinplot(x=x, y=y, hue=hue, data=tips)
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
 
+tips = sns.load_dataset('tips')
+
+def plots(x, y, hue, plot):
+    if plot=='Strip plot': sns.stripplot(x=x, y=y, hue=hue, data=tips)
+    if plot=='Box plot': sns.boxplot(x=x, y=y, hue=hue, data=tips)
+    if plot=='Swarm plot': sns.swarmplot(x=x, y=y, hue=hue, data=tips)
+    if plot=='Violin plot': sns.violinplot(x=x, y=y, hue=hue, data=tips)
+
+st.title('Visualizing the tips dataset')
 
 plots = st.selectbox(
      'Select plot',
@@ -29,4 +30,7 @@ y = st.selectbox(
      'Select numerical variable',
      ('tip','total_bill'))
 
-st.title('Tips dataset')
+if plots = 'Violin plot': plots(x,y,hue,'Strip plot')
+elif plots = 'Box plot': plots(x,y,hue,'Box plot')
+elif plots = 'Swarm plot': plots(x,y,hue,'Swarm plot')
+else: plots(x,y,hue,'Violin plot')
