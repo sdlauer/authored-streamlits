1f633cdb31635f0bf5f9db56fe4abac4b5a3856d
Chris Chan <chris.chan@zybooks.com>
2022-09-03 10:19:00 -0500

    Add files via upload

--- tips_plots.py
+++ tips_plots.py
@@ -1,39 +1,3 @@
-# import streamlit as st
-# import pandas as pd
-# import seaborn as sns
-# import numpy as np
-# import matplotlib.pyplot as plt
-#
-# tips = sns.load_dataset('tips')
-#
-# # def plots(x, y, hue, plot):
-# #     fig = plt.figure(figsize=(12, 6))
-# #     if plot=='Strip plot': sns.stripplot(x=x, y=y, data=tips)
-# #     if plot=='Box plot': sns.boxplot(x=x, y=y, hue=hue, data=tips)
-# #     if plot=='Swarm plot': sns.swarmplot(x=x, y=y, hue=hue, data=tips)
-# #     if plot=='Violin plot': sns.violinplot(x=x, y=y, hue=hue, data=tips)
-# #     st.pyplot(fig)
-#
-# st.title('Visualizing the tips dataset')
-#
-# plots = st.selectbox(
-#      'Select plot',
-#      ('Strip plot', 'Box plot', 'Swarm plot','Violin plot'))
-#
-# hue = st.selectbox(
-#      'Select hue',
-#      (None,'sex', 'smoker', 'day','time'))
-#
-# x = st.selectbox(
-#      'Select categorical variable',
-#      ('sex', 'smoker', 'day','time'))
-#
-# y = st.selectbox(
-#      'Select numerical variable',
-#      ('tip','total_bill'))
-#
-# if plots == 'Strip plot': sns.stripplot(x='day', y='tips', data=tips)
-
 import streamlit as st
 import pandas as pd
 import seaborn as sns
@@ -55,20 +19,12 @@ plot = st.selectbox(
 
 categorical = st.selectbox(
     "Categorical variable",
-    [
-        "day",
-        "time",
-        "sex",
-        "smoker"
-    ]
+    ["day","time","sex","smoker"]
 )
 
 numeric = st.selectbox(
     "Numeric variable",
-    [
-        "tip",
-        "total_bill"
-    ]
+    ["tip","total_bill"]
 )
 
 # group = st.selectbox(
@@ -90,7 +46,7 @@ if plot == "violin plot":
 elif plot == "strip plot":
     sns.stripplot(x=categorical, y=numeric, data = tips)
 
-elif plot == "Box plot":
+elif plot == "box plot":
     sns.boxplot(x=categorical, y=numeric, data = tips)
 
 else:
