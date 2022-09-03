c6b8872d57e4eb1a73ba9cd8afb5b5c92a5d6154
Chris Chan <chris.chan@zybooks.com>
2022-09-03 10:50:05 -0500

    Add files via upload

---
+++ mpg_regression.py
@@ -0,0 +1,44 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import streamlit as st
+
+mpg = pd.read_csv("mpg.csv")
+
+reg_line = st.checkbox("Regression line")
+reg_eq = st.checkbox("Regression equation")
+corr_coef = st.checkbox("Correlation coefficient")
+
+input = st.selectbox(
+    "Input feature",
+    [
+        'mpg',
+        'cylinders',
+        'displacement',
+        'horsepower',
+        'weight',
+        'acceleration',
+        'model_year',
+        'origin',
+        'name'
+    ]
+)
+
+output = st.selectbox(
+    "Output feature",
+    [
+        'mpg',
+        'cylinders',
+        'displacement',
+        'horsepower',
+        'weight',
+        'acceleration',
+        'model_year',
+        'origin',
+        'name'
+    ]
+)
+
+fig = plt.figure()
+sns.scatterplot(x=input, y=output, data=mpg)
+st.pyplot(fig)
