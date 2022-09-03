8fb746b5fe58746ffcacc452803655ce0723cc3a
cjschan <cjschan@gmail.com>
2022-09-03 16:59:36 -0500

    Update country_manip.py

--- country_manip.py
+++ country_manip.py
@@ -3,9 +3,19 @@ import pandas as pd
 
 country = pd.read_csv("country.csv")
 
+st.title("Manipulating the country dataset")
+
 code = st.checkbox("Code")
 name = st.checkbox("Name")
 continent = st.checkbox("Continent")
 population = st.checkbox("Population")
 
-st.title("Manipulating the country dataset")
+list = []
+if code: list.append()
+if name: list.append()
+if continent: list.append()
+if population: list.append()
+
+country_sub = country[list]
+
+st.dataframe(country_sub)
