0048591f2a09337203916c2898d93af6ecea55d4
cjschan <cjschan@gmail.com>
2022-09-03 17:05:06 -0500

    Update country_manip.py

--- country_manip.py
+++ country_manip.py
@@ -6,9 +6,9 @@ country = pd.read_csv("country.csv")
 st.title("Manipulating the country dataset")
 
 code = st.checkbox("Code", value=True)
-name = st.checkbox("Name", value=True))
-continent = st.checkbox("Continent", value=True))
-population = st.checkbox("Population", value=True))
+name = st.checkbox("Name", value=True)
+continent = st.checkbox("Continent", value=True)
+population = st.checkbox("Population", value=True)
 
 list = []
 if code: list.append("Code")
