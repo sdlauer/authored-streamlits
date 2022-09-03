a0685597753790272dd02f8058bb2d6ede3116fe
Chris Chan <chris.chan@zybooks.com>
2022-09-03 11:18:55 -0500

    Add files via upload

--- mpg_regression.py
+++ mpg_regression.py
@@ -34,6 +34,40 @@ reg_line = st.checkbox("Regression line")
 reg_eq = st.checkbox("Regression equation")
 corr_coef = st.checkbox("Correlation coefficient")
 
+# Plot linear regression line
+def add_reg(data):
+    m, b = np.polyfit(data[0], data[1], 1)
+    x = [1,100]
+    y = [m*x[0]+b, m*x[1]+b]
+    plt.plot(x,y, c='red')
+
+# Add residuals to the plot
+def add_resid(data):
+    n = len(data[0])
+    m, b = np.polyfit(data[0], data[1], 1)
+    y_pred = m*data[0] + b
+    for i in range(n):
+        plt.plot([data[0][i],data[0][i]],[data[1][i],y_pred[i]], c = 'black')
+
+# Show linear regression equation
+def show_eq(data):
+    m, b = np.polyfit(data[0], data[1], 1)
+    m = round(m,3)
+    b = round(b,3)
+    plt.text(110,0,'y =' + str(m) + 'x + ' + str(b))
+
+# Show correlation coefficient
+def show_corr(data):
+    # Note: np.corrcoef gives a correlation matrix
+    corr_coef = np.corrcoef(data[0],data[1])
+    corr_coef = round(corr_coef[0,1],3)
+    plt.text(110,2,'r = ' + str(corr_coef))
+
+if line: add_reg(mpg[input_feat],mpg[output_feat])
+if resid: add_resid(mpg[input_feat],mpg[output_feat])
+if equation: show_eq(mpg[input_feat],mpg[output_feat])
+if corr: show_corr(mpg[input_feat],mpg[output_feat])
+
 fig = plt.figure()
 sns.scatterplot(x=input_feat, y=output_feat, data=mpg)
 st.pyplot(fig)
