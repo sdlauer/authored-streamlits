3b7f9cc2eb93c795cd65bf419fb31bb6b455a396
cjschan <cjschan@gmail.com>
2022-09-03 16:52:23 -0500

    Update country_manip.py

--- country_manip.py
+++ country_manip.py
@@ -1,2 +1,11 @@
 import streamlit as st
 import pandas as pd
+
+country = pd.read_csv("country.csv")
+
+code = st.checkbox("Code")
+name = st.checkbox("Name")
+continent = st.checkbox("Continent")
+population = st.checkbox("Population")
+
+st.title("Manipulating the country dataset")
