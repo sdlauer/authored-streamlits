bc51d2b483cdebeac3f07d0d08627a608d29f482
cjschan <cjschan@gmail.com>
2022-09-04 17:03:36 -0500

    Adding files

--- country_manip.py
+++ country_manip.py
@@ -1,52 +1,3 @@
-# import streamlit as st
-# import pandas as pd
-#
-# country = pd.read_csv("country.csv")
-#
-# st.title("Manipulating the country dataset")
-#
-# st.subheader("Display column(s):")
-# code = st.checkbox("Code", value=True)
-# name = st.checkbox("Name", value=True)
-# continent = st.checkbox("Continent", value=True)
-# population = st.checkbox("Population", value=True)
-#
-# list = []
-# if code: list.append("Code")
-# if name: list.append("Name")
-# if continent: list.append("Continent")
-# if population: list.append("Population")
-#
-# country_sub = country[list]
-#
-# group = st.selectbox(
-#     "Display by continent",
-#     [
-#         None,
-#         "Asia",
-#         "Europe",
-#         "Africa",
-#         "Oceania",
-#         "North America",
-#         "Antarctica",
-#         "South America"
-#     ]
-# )
-#
-# if continent:
-#     if group!=None:
-#         country_sub = country_sub[country_sub["Continent"] == group]
-#         st.dataframe(country_sub)
-#         st.subheader("Mean population of all countries in " + group)
-#         st.text(country_sub.mean().round(0))
-#     elif group==None:
-#         st.dataframe(country_sub)
-#         st.subheader("Mean population of all countries")
-#         st.text(country_sub.mean().round(0))
-# else:
-#     st.dataframe(country_sub)
-
-
 import pandas as pd
 import streamlit as st
 from st_aggrid import AgGrid, GridOptionsBuilder
@@ -95,7 +46,3 @@ country = pd.read_csv(
 )
 
 selection = aggrid_interactive_table(df=country)
-
-if selection:
-    st.write("You selected:")
-    st.json(selection["selected_rows"])
---
+++ country_test.py
@@ -0,0 +1,47 @@
+import streamlit as st
+import pandas as pd
+
+country = pd.read_csv("country.csv")
+
+st.title("Manipulating the country dataset")
+
+st.subheader("Display column(s):")
+code = st.checkbox("Code", value=True)
+name = st.checkbox("Name", value=True)
+continent = st.checkbox("Continent", value=True)
+population = st.checkbox("Population", value=True)
+
+list = []
+if code: list.append("Code")
+if name: list.append("Name")
+if continent: list.append("Continent")
+if population: list.append("Population")
+
+country_sub = country[list]
+
+group = st.selectbox(
+    "Display by continent",
+    [
+        None,
+        "Asia",
+        "Europe",
+        "Africa",
+        "Oceania",
+        "North America",
+        "Antarctica",
+        "South America"
+    ]
+)
+
+if continent:
+    if group!=None:
+        country_sub = country_sub[country_sub["Continent"] == group]
+        st.dataframe(country_sub)
+        st.subheader("Mean population of all countries in " + group)
+        st.text(country_sub.mean().round(0))
+    elif group==None:
+        st.dataframe(country_sub)
+        st.subheader("Mean population of all countries")
+        st.text(country_sub.mean().round(0))
+else:
+    st.dataframe(country_sub)
