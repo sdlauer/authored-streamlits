d1d252609e02fa29808c1adebb065252d5aa4fa2
cjschan <cjschan@gmail.com>
2022-09-04 08:05:19 -0500

    Update mpg_regress.py

--- mpg_regress.py
+++ mpg_regress.py
@@ -4,40 +4,10 @@ import seaborn as sns
 import numpy as np
 import matplotlib.pyplot as plt
 
+# Load dataset
 mpg = pd.read_csv("mpg.csv")
 
-st.title("Regression using the mpg dataset")
-
-input_feat = st.selectbox(
-    "Input feature",
-    [
-        "mpg",
-        "cylinders",
-        "horsepower",
-        "displacement",
-        "weight",
-        "acceleration",
-        "model_year"
-    ]
-)
-
-output_feat = st.selectbox(
-    "Output feature",
-    [
-        "mpg",
-        "cylinders",
-        "horsepower",
-        "displacement",
-        "weight",
-        "acceleration",
-        "model_year"
-    ]
-)
-
-reg_line = st.checkbox("Regression line")
-reg_eq = st.checkbox("Regression equation")
-corr_coef = st.checkbox("Correlation coefficient")
-
+# Functions for equation and correlation
 def show_eq(data):
     m, b = np.polyfit(data[0], data[1], 1)
     m = round(m,3)
@@ -52,18 +22,56 @@ def show_corr(data):
     corr = 'r = ' + str(corr_coef)
     return corr
 
-fig, ax = plt.subplots()
+st.title("Regression using the mpg dataset")
 
-ax = sns.regplot(x=input_feat, y=output_feat,
-    data=mpg, fit_reg=reg_line, ci=None, line_kws={"color": "grey"})
+col1, col2 = st.columns([1,3])
 
-if reg_eq:
-    st.subheader("Regression equation")
-    st.text(show_eq([mpg[input_feat],mpg[output_feat]]))
+with col1:
 
-if corr_coef:
-    st.subheader("Correlation coefficient")
-    st.text(show_corr([mpg[input_feat],mpg[output_feat]]))
+    input_feat = st.selectbox(
+        "Input feature",
+        [
+            "mpg",
+            "cylinders",
+            "horsepower",
+            "displacement",
+            "weight",
+            "acceleration",
+            "model_year"
+        ]
+    )
 
-st.subheader("Scatter plot")
-st.pyplot(fig)
+    output_feat = st.selectbox(
+        "Output feature",
+        [
+            "mpg",
+            "cylinders",
+            "horsepower",
+            "displacement",
+            "weight",
+            "acceleration",
+            "model_year"
+        ]
+    )
+
+    reg_line = st.checkbox("Regression line")
+    reg_eq = st.checkbox("Regression equation")
+    corr_coef = st.checkbox("Correlation coefficient")
+
+with col2:
+    
+    fig, ax = plt.subplots()
+
+    ax = sns.regplot(x=input_feat, y=output_feat,
+        data=mpg, fit_reg=reg_line, ci=None, line_kws={"color": "grey"})
+
+    if reg_eq:
+        st.subheader("Regression equation")
+        st.text(show_eq([mpg[input_feat],mpg[output_feat]]))
+
+    if corr_coef:
+        st.subheader("Correlation coefficient")
+        st.text(show_corr([mpg[input_feat],mpg[output_feat]]))
+
+    st.subheader("Scatter plot")
+    st.pyplot(fig)
