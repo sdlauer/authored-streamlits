ea1b5be7b50b28a4722c667a6001fb05b5f47e53
cjschan <cjschan@gmail.com>
2022-09-06 11:31:03 -0500

    Update tips_plots.py

--- tips_plots.py
+++ tips_plots.py
@@ -9,18 +9,7 @@ hide = """
         #MainMenu {visibility: hidden;}
         footer {visibility: hidden;}
         header {visibility: hidden;}
-        .css-18e3th9 {
-                    padding-top: 0rem;
-                    padding-bottom: 10rem;
-                    padding-left: 5rem;
-                    padding-right: 5rem;
-                }
-        .css-1d391kg {
-                    padding-top: 3.5rem;
-                    padding-right: 1rem;
-                    padding-bottom: 3.5rem;
-                    padding-left: 1rem;
-                }
+        div.block-container {padding-top:1rem;}
         </style>
         """
 
