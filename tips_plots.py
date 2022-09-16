8abd1e0254ec2c9f8e3fc2bda63ae0cc1d4c9fd8
cjschan <cjschan@gmail.com>
2022-09-16 13:27:01 -0500

    Update tips_plots.py

--- tips_plots.py
+++ tips_plots.py
@@ -79,18 +79,18 @@ with col2:
     fig = plt.figure()
 
     if plot == "Violin plot":
-        sns.violinplot(x=dict[categorical], y=dict[numeric], hue=dict[categorical], data = tips)
+        sns.violinplot(x=dict[categorical], y=dict[numeric], hue=dict[Grouping], data = tips)
 
     elif plot == "Density plot":
-        sns.kdeplot(x=dict[numeric], hue=dict[categorical], data = tips)
+        sns.kdeplot(x=dict[numeric], hue=dict[Grouping], data = tips)
 
     elif plot == "Strip plot":
-        sns.stripplot(x=dict[categorical], y=dict[numeric], hue=dict[categorical], data = tips)
+        sns.stripplot(x=dict[categorical], y=dict[numeric], hue=dict[Grouping], data = tips)
 
     elif plot == "Box plot":
-        sns.boxplot(x=dict[categorical], y=dict[numeric], hue=dict[categorical], data = tips)
+        sns.boxplot(x=dict[categorical], y=dict[numeric], hue=dict[Grouping], data = tips)
 
     else:
-        sns.swarmplot(x=dict[categorical], y=dict[numeric], hue=dict[categorical], data = tips)
+        sns.swarmplot(x=dict[categorical], y=dict[numeric], hue=dict[Grouping], data = tips)
 
     st.pyplot(fig)
