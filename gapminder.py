4114bfd093edb9d81482b969187a85842b9b72d2
cjschan <cjschan@gmail.com>
2022-09-17 17:27:05 -0500

    Create gapminder.py

---
+++ gapminder.py
@@ -0,0 +1,56 @@
+import streamlit as st
+import pandas as pd
+import seaborn as sns
+import numpy as np
+import matplotlib.pyplot as plt
+
+hide = """
+        <style>
+        #MainMenu {visibility: hidden;}
+        footer {visibility: hidden;}
+        header {visibility: hidden;}
+        body {overflow: hidden;}
+        div.block-container {padding-top:1rem;}
+        div.block-container {padding-bottom:1rem;}
+        </style>
+        """
+
+st.markdown(hide, unsafe_allow_html=True)
+
+gapminder = pd.read_csv("gapminder.csv")
+
+# st.header("Visualizing the tips dataset")
+
+col1, col2 = st.columns([1,3])
+
+with col1:
+    plot = st.selectbox(
+        "Plot",
+        [
+            "Box plot",
+            "Histogram"
+        ]
+    )
+
+    numerical = st.selectbox(
+        "Numerical feature",
+        [
+            "Population",
+            "GDP",
+            "Life expectancy"
+        ]
+    )
+
+
+with col2:
+    fig, ax = plt.subplots()
+
+    if plot == "Box plot":
+        sns.boxplot(x=numerical, data=gapminder)
+
+    elif plot == "Histogram":
+        sns.histplot(x=numerical, data=gapminder)
+
+    ax.set_xlabel(numerical, fontsize=14)
+
+    st.pyplot(fig)
