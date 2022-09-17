795aa3e5fc35ac6d3ef24f85a0a51c8831e2d928
cjschan <cjschan@gmail.com>
2022-09-17 14:24:55 -0500

    Update tips_bars.py

--- tips_bars.py
+++ tips_bars.py
@@ -65,12 +65,12 @@ with col2:
 
 
     if plot == "Grouped":
-        pd.crosstab(tips[group], tips[categorical]).plot(kind="bar")
-        sns.countplot(x=categorical, hue=group, data = tips)
+        pd.crosstab(tips['Day'], tips['Smoker']).plot(kind='bar')
+        #sns.countplot(x=categorical, hue=group, data = tips)
 
     else:
-        pd.crosstab(tips[group], tips[categorical]).plot(kind="bar", stacked=True)
-        sns.countplot(x=categorical, hue=group, data = tips)
+        # pd.crosstab(tips[group], tips[categorical]).plot(kind="bar", stacked=True)
+        # sns.countplot(x=categorical, hue=group, data = tips)
 
     ax.set_xlabel(categorical, fontsize=14)
     ax.set_ylabel("Count", fontsize=14)
