248a09607969d873e40815b17ad5e2c33158928c
cjschan <cjschan@gmail.com>
2022-09-17 13:29:26 -0500

    Create tips_bars.py

---
+++ tips_bars.py
@@ -0,0 +1,71 @@
+import streamlit as st
+import pandas as pd
+import seaborn as sns
+import numpy as np
+import matplotlib.pyplot as plt
+
+hide = """
+        <style>
+        #MainMenu {visibility: hidden;}
+        footer {visibility: hidden;}
+        header {visibility: hidden;}
+        body {overflow: hidden;}
+        div.block-container {padding-top:1rem;}
+        div.block-container {padding-bottom:1rem;}
+        </style>
+        """
+
+st.markdown(hide, unsafe_allow_html=True)
+
+tips = sns.load_dataset('tips')
+tips.columns = ["Total bill", "Tip", "Sex", "Smoker", "Day", "Time", "Party size"]
+
+# st.header("Visualizing the tips dataset")
+
+col1, col2 = st.columns([1,3])
+
+with col1:
+    plot = st.selectbox(
+        "Plot",
+        [
+            "Stacked count plot",
+            "Grouped count plot"
+        ]
+    )
+
+
+
+    categorical = st.selectbox(
+        "Categorical variable",
+        [
+            "Day",
+            "Party size",
+            "Time",
+            "Sex",
+            "Smoker"
+        ]
+    )
+
+
+    group = st.selectbox(
+        "Grouping",
+        [
+            None,
+            "Day",
+            "Party size",
+            "Time",
+            "Sex",
+            "Smoker"
+        ]
+    )
+
+with col2:
+    fig, ax = plt.subplots()
+
+    if plot == "Grouped count plot":
+        sns.countplot(x=categorical, hue=group, data = tips)
+
+    else:
+        sns.countplot(x=categorical, hue=group, data = tips)
+
+    st.pyplot(fig)
