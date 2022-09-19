94d106b8d0a3698de4c7287a26d8a5e5d028f9e8
cjschan <cjschan@gmail.com>
2022-09-19 12:14:17 -0500

    Update tips_bars.py

--- tips_bars.py
+++ tips_bars.py
@@ -20,75 +20,72 @@ st.markdown(hide, unsafe_allow_html=True)
 tips = sns.load_dataset('tips')
 tips.columns = ["Total bill", "Tip", "Sex", "Smoker", "Day", "Time", "Party size"]
 
-# st.header("Visualizing the tips dataset")
-
-tab1, tab2 = st.tabs(["Plot", "Cross tabulation"])
-
 with tab1:
-    col1, col2 = st.columns([2,3])
+col1, col2 = st.columns([2,3])
 
-    with col1:
+with col1:
 
-        type = st.selectbox(
-            "Categorical feature",
-            [
-                "Stacked",
-                "Grouped"
-            ]
-        )
+    type = st.selectbox(
+        "Categorical feature",
+        [
+            "Stacked",
+            "Grouped"
+        ]
+    )
 
-        categorical = st.selectbox(
-            "Categorical feature",
-            [
-                "Day",
-                "Party size",
-                "Time",
-                "Sex",
-                "Smoker"
-            ]
-        )
+    categorical = st.selectbox(
+        "Categorical feature",
+        [
+            "Day",
+            "Party size",
+            "Time",
+            "Sex",
+            "Smoker"
+        ]
+    )
 
-        if categorical=="Day":
-                group = st.selectbox(
-                    "Grouping",
-                    ["Party size","Time","Sex","Smoker"]
-                )
+    if categorical=="Day":
+            group = st.selectbox(
+                "Grouping",
+                ["Party size","Time","Sex","Smoker"]
+            )
 
-        elif categorical=="Party size":
-                group = st.selectbox(
-                    "Grouping",
-                    ["Day","Time","Sex","Smoker"]
-                )
+    elif categorical=="Party size":
+            group = st.selectbox(
+                "Grouping",
+                ["Day","Time","Sex","Smoker"]
+            )
 
-        elif categorical=="Time":
-                group = st.selectbox(
-                    "Grouping",
-                    ["Day","Party size","Sex","Smoker"]
-                )
+    elif categorical=="Time":
+            group = st.selectbox(
+                "Grouping",
+                ["Day","Party size","Sex","Smoker"]
+            )
 
-        elif categorical=="Sex":
-                group = st.selectbox(
-                    "Grouping",
-                    ["Day","Party size","Smoker","Time"]
-                )
+    elif categorical=="Sex":
+            group = st.selectbox(
+                "Grouping",
+                ["Day","Party size","Smoker","Time"]
+            )
 
-        else:
-                group = st.selectbox(
-                    "Grouping",
-                    ["Day","Party size","Sex","Time"]
-                )
+    else:
+            group = st.selectbox(
+                "Grouping",
+                ["Day","Party size","Sex","Time"]
+            )
 
+    check = st.checkbox("Display cross tabulation")
 
-    with col2:
-        fig, ax = plt.subplots()
-        if type=="Stacked": sns.histplot(x=categorical, hue=group, data=tips, shrink=.8, multiple="stack")
-        elif type=="Grouped": sns.histplot(x=categorical, hue=group, data=tips, shrink=.8, multiple="dodge")
-
-        ax.set_xlabel(categorical, fontsize=14)
-        ax.set_ylabel("Count", fontsize=14)
-
-        st.pyplot(fig)
-
-with tab2:
+    if check:
         cross = pd.crosstab(tips[categorical], tips[group])
         st.dataframe(cross)
+
+with col2:
+    fig, ax = plt.subplots()
+    if type=="Stacked": sns.histplot(x=categorical, hue=group, data=tips, shrink=.8, multiple="stack")
+    elif type=="Grouped": sns.histplot(x=categorical, hue=group, data=tips, shrink=.8, multiple="dodge")
+
+    ax.set_xlabel(categorical, fontsize=14)
+    ax.set_ylabel("Count", fontsize=14)
+
+    st.pyplot(fig)
