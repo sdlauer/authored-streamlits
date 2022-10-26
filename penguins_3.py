c8a01078e42d9ab80c0065401cdd2f1dee950d3b
cjschan <cjschan@gmail.com>
2022-10-26 16:58:24 -0500

    Create penguins_3.py

---
+++ penguins_3.py
@@ -0,0 +1,49 @@
+import streamlit as st
+import pandas as pd
+import seaborn as sns
+import numpy as np
+import matplotlib.pyplot as plt
+
+hide = """
+        <style>
+        #MainMenu {visibility: hidden;}
+        footer {visibility: hidden;}
+        header {visibility: hidden;}
+        body {overflow: hidden;}
+        div.block-container {padding-top:1rem;}
+        div.block-container {padding-bottom:1rem;}
+        </style>
+        """
+
+penguins = sns.load_dataset('penguins')
+penguins.columns = ["species", "island", "bill_length_mm", "bill_depth_mm", "flipper_length_mm", "body_mass_g", "sex"]
+col1, col2 = st.columns([1,3])
+
+
+tab1, tab2 = st.tabs(["Plot", "Summary statistics"])
+
+with tab1:
+    col1, col2 = st.columns([1.5,3])
+
+    with col1:
+        numerical = st.selectbox(
+            "Numerical feature",
+            ["bill_length_mm", "bill_depth_mm", "flipper_length_mm", "body_mass_g"]
+        )
+
+        categorical = st.selectbox(
+            "Continent",
+            ["species", "island","sex"]
+        )
+
+    with col2:
+        sns.histplot(x=df, kde=True, stat="density")
+        ax.set_xlabel(numerical, fontsize=14)
+        ax.set_ylabel("Density", fontsize=14)
+        ax.ticklabel_format(style='plain', axis='y')
+        st.pyplot(fig)
+
+with tab2:
+        st.subheader("Summary statistics")
+        summary = penguins[penguins[categorical]==categorical].describe()
+        st.dataframe(summary)
