5196f118986d621b87b9db7dd466a6ed46840c01
cjschan <cjschan@gmail.com>
2022-10-26 16:00:39 -0500

    Update penguins_1.py

--- penguins_1.py
+++ penguins_1.py
@@ -20,75 +20,75 @@ st.markdown(hide, unsafe_allow_html=True)
 penguins = sns.load_dataset('penguins')
 
 
-penguins.columns = ["Species", "Island", "Bill length", "Bill depth", "Flipper length", "Body mass", "Sex"]
+penguins.columns = ["species", "island", "bill_length_mm", "bill_depth_mm", "flipper_length_mm", "body_mass_g", "sex"]
 
 col1, col2 = st.columns([1,3])
 
 with col1:
-    categorical_1 = st.selectbox(
+    numerical_1 = st.selectbox(
         "Select numerical feature",
         [
-            "Bill length",
-            "Bill depth",
-            "Flipper length",
-            "Body mass"
+            "bill_length_mm",
+            "bill_depth_mm",
+            "flipper_length_mm",
+            "body_mass_g"
         ]
     )
 
-    if (categorical_1=="Bill length"):
-            categorical_2 = st.selectbox(
+    if (numerical_1=="bill_length_mm"):
+            numerical_2 = st.selectbox(
                 "Select another numerical feature",
-                ["Bill depth", "Flipper length", "Body mass"]
+                ["bill_depth_mm", "flipper_length_mm", "body_mass_g"]
             )
 
-    elif (categorical_1=="Bill depth"):
-            categorical_2 = st.selectbox(
+    elif (numerical_1=="bill_depth_mm"):
+            numerical_2 = st.selectbox(
                 "Select another numerical feature",
-                ["Bill length", "Flipper length", "Body mass"]
+                ["bill_length_mm", "flipper_length_mm", "body_mass_g"]
             )
 
-    elif (categorical_1=="Flipper length"):
-            categorical_2 = st.selectbox(
+    elif (numerical_1=="flipper_length_mm"):
+            numerical_2 = st.selectbox(
                 "Select another numerical feature",
-                ["Bill length", "Bill depth", "Body mass"]
+                ["bill_length_mm", "bill_depth_mm", "body_mass_g"]
             )
 
-    elif (categorical_1=="Body mass"):
-            categorical_2 = st.selectbox(
+    elif (numerical_1=="body_mass_g"):
+            numerical_2 = st.selectbox(
                 "Select another numerical feature",
-                ["Bill length", "Bill depth", "Flipper length"]
+                ["bill_length_mm", "bill_depth_mm", "flipper_length_mm"]
             )
 
     grouping_1 = st.selectbox(
             "Select color grouping",
             [
-                "Species",
-                "Island",
-                "Sex"
+                "species",
+                "island",
+                "sex"
             ]
         )
 
-    if (grouping_1=="Species"):
+    if (grouping_1=="species"):
         grouping_2 = st.selectbox(
             "Select style grouping",
-            ["Island","Sex"]
+            ["island","sex"]
         )
 
-    elif (grouping_1=="Island"):
+    elif (grouping_1=="island"):
         grouping_2 = st.selectbox(
             "Select style grouping",
-            ["Species","Sex"]
+            ["species","sex"]
         )
 
-    elif (grouping_1=="Sex"):
+    elif (grouping_1=="sex"):
         grouping_2 = st.selectbox(
             "Select style grouping",
-            ["Species","Island"]
+            ["species","island"]
         )
 
 with col2:
     fig, ax = plt.subplots()
-    sns.scatterplot(x=categorical_1, y=categorical_2, hue=grouping_1, style=grouping_2, data = penguins)
-    ax.set_xlabel(categorical_1, fontsize=14)
-    ax.set_ylabel(categorical_2, fontsize=14)
+    sns.scatterplot(x=numerical_1, y=numerical_2, hue=grouping_1, style=grouping_2, data = penguins)
+    ax.set_xlabel(numerical_1, fontsize=14)
+    ax.set_ylabel(numerical_2, fontsize=14)
     st.pyplot(fig)
