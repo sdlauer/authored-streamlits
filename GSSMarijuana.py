31d9f366f7d569f4aa27a01592f36857e9a2e4a1
Matt Rissler <mrissler@wiley.com>
2022-10-26 21:07:39 -0500

    fix import

--- GSSMarijuana.py
+++ GSSMarijuana.py
@@ -11,7 +11,7 @@ import pandas as pd
 #import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.tree import DecisionTreeClassifier, plot_tree#, export_text
-#from sklearn import metrics, tree
+from sklearn import metrics, tree
 #from sklearn.model_selection import train_test_split
 #from sklearn.ensemble import RandomForestClassifier
 
