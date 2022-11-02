b883b9ce0ad520cb171f2c745d656f4fcf43517d
Matt Rissler <mrissler@wiley.com>
2022-11-02 14:01:44 -0500

    Indents?

--- GSSMarijuana.py
+++ GSSMarijuana.py
@@ -79,6 +79,7 @@ else:
     #fig, ax = plt.subplots()
     if depth > 2:
         st.text("Right-click to open image in a new tab for a larger view.")
+        
     fig = plt.figure(figsize=(depth*16,depth*9))
     plot_tree(classtreeModel, feature_names=X.columns,
                        filled=True, fontsize=None, )
