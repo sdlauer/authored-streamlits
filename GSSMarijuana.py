cccad977954ff6ec62b8cfae53bf4d24eab5d606
Matt Rissler <mrissler@wiley.com>
2022-11-02 13:49:46 -0500

    playing with size!

--- GSSMarijuana.py
+++ GSSMarijuana.py
@@ -78,7 +78,7 @@ if text:
     st.text(export_text(classtreeModel))
 else:
     #fig, ax = plt.subplots()
-    fig = plt.figure(figsize=(50,30))
+    fig = plt.figure(figsize=(20,15))
     plot_tree(classtreeModel, feature_names=X.columns,
                        filled=True, fontsize=8, )
     st.pyplot(fig)
