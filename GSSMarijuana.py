f1956ae27ae45bd0e68a73fbc6eab5137f6818c5
Matt Rissler <mrissler@wiley.com>
2022-11-03 11:04:07 -0500

    Aspect ratio

--- GSSMarijuana.py
+++ GSSMarijuana.py
@@ -68,7 +68,7 @@ with col2:
             st.write(metrics.confusion_matrix(y, y_pred))
         else:
             disp = metrics.ConfusionMatrixDisplay.from_predictions(y, y_pred)
-            fig, ax = plt.subplots(figsize=(4,4))
+            fig, ax = plt.subplots(figsize=(4,3))
             disp.plot(ax=ax)
             st.pyplot(fig)
 
