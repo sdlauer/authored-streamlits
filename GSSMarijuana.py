60250fdfc0da9ebe7928d06d1da2f1f50f988bff
Matt Rissler <mrissler@wiley.com>
2022-11-03 11:05:28 -0500

    ratio

--- GSSMarijuana.py
+++ GSSMarijuana.py
@@ -68,7 +68,7 @@ with col2:
             st.write(metrics.confusion_matrix(y, y_pred))
         else:
             disp = metrics.ConfusionMatrixDisplay.from_predictions(y, y_pred)
-            fig, ax = plt.subplots(figsize=(4,3))
+            fig, ax = plt.subplots(figsize=(4,2))
             disp.plot(ax=ax)
             st.pyplot(fig)
 
