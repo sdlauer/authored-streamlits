3903505382c1cc3a82891e116de72aee44093514
Matt Rissler <mrissler@wiley.com>
2022-11-03 11:12:07 -0500

    small font fix

--- GSSMarijuana.py
+++ GSSMarijuana.py
@@ -68,9 +68,11 @@ with col2:
             st.write(metrics.confusion_matrix(y, y_pred))
         else:
             disp = metrics.ConfusionMatrixDisplay.from_predictions(y, y_pred)
-            #fig, ax = plt.subplots(figsize=(4,2))
-            #disp.plot(ax=ax)
-            st.pyplot(disp.figure_)
+            fig, ax = plt.subplots(figsize=(1,1))
+            disp.plot(ax=ax)
+            st.pyplot(fig)
+            #st.pyplot(disp.figure_)
+
 
 #Plot the tree
 st.header("Classification tree")
