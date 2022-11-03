bf2a698ed2cba16ace8f4caef970b5ae55926505
Matt Rissler <mrissler@wiley.com>
2022-11-03 08:57:57 -0500

    40s

--- GSSMarijuana.py
+++ GSSMarijuana.py
@@ -86,7 +86,7 @@ else:
 
     else:
         plot_tree(classtreeModel, feature_names=X.columns,
-                  filled=True, fontsize=38, )
+                  filled=True, fontsize=40, )
 
     st.pyplot(fig)
 
