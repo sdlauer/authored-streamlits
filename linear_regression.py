47c99d545f82e4c7bafbe5c3d78d37abbbc3ae0a
cjschan <cjschan@gmail.com>
2022-11-07 12:32:56 -0600

    Update linear_regression.py

--- linear_regression.py
+++ linear_regression.py
@@ -97,8 +97,7 @@ with col2:
         m, b = np.polyfit(np.ravel(X).astype(int), np.ravel(y).astype(int), 1)
         m = np.round(m,3)
         b = np.round(b,3)
-        st.text(m)
-        st.text(b)
+        st.latex("\hat{" + target + "} = " + str(m) + "(Latitude)" + str(b))
     # regModeleq = st.checkbox("Display regression model")
     # if regModeleq: show_eq(X,y,target)
 
