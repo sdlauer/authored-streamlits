7b1a39ffa08cfd55aa30760c8fb4e2c63f5d9a49
cjschan <cjschan@gmail.com>
2022-11-07 11:04:11 -0600

    Create linear_regression.py

---
+++ linear_regression.py
@@ -0,0 +1,90 @@
+import streamlit as st
+import pandas as pd
+import seaborn as sns
+import numpy as np
+import matplotlib.pyplot as plt
+
+hide = """
+        <style>
+        #MainMenu {visibility: hidden;}
+        footer {visibility: hidden;}
+        header {visibility: hidden;}
+        body {overflow: hidden;}
+        div.block-container {padding-top:1rem;}
+        div.block-container {padding-bottom:1rem;}
+        </style>
+        """
+
+st.markdown(hide, unsafe_allow_html=True)
+
+crabs = pd.read_csv("crab-groups.csv")
+
+tips.columns = ["Site", "Latitude", "Sample size", "Mean length", "Min length", "Max length", "Stdev length","Median length","Date"]
+
+# Show linear regression equation
+def show_eq(X,y,target):
+    m, b = np.polyfit(X, y, 1)
+    m = round(m,3)
+    b = round(b,3)
+    st.latex('\hat{\text{' + target + '}} = ' + str(m) + '(\text{Latitude}) + ' + str(b)))
+
+# Plot linear regression line
+def add_reg(data):
+    m, b = np.polyfit(data[0], data[1], 1)
+    x = [1,100]
+    y = [m*x[0]+b, m*x[1]+b]
+    plt.plot(x,y, c='red')
+
+# Add residuals to the plot
+def add_resid(data):
+    n = len(data[0])
+    m, b = np.polyfit(data[0], data[1], 1)
+    y_pred = m*data[0] + b
+    for i in range(n):
+        plt.plot([data[0][i],data[0][i]],[data[1][i],y_pred[i]], c = 'black')
+
+# Show linear regression equation
+def show_eq(data):
+    m, b = np.polyfit(data[0], data[1], 1)
+    m = round(m,3)
+    b = round(b,3)
+    plt.text(110,0,'y =' + str(m) + 'x + ' + str(b))
+
+# Show correlation coefficient
+def show_corr(data):
+    # Note: np.corrcoef gives a correlation matrix
+    corr_coef = np.corrcoef(data[0],data[1])
+    corr_coef = round(corr_coef[0,1],3)
+    plt.text(110,2,'r = ' + str(corr_coef))
+
+
+col1, col2 = st.columns([1,3])
+
+with col1:
+    target = st.selectbox(
+        "Select target feature",
+        [
+            "Mean length",
+            "Min length",
+            "Max length",
+            "Median length"
+        ]
+    )
+
+    # Store relevant columns as variables
+    X = crabs[['Latitude']].values.reshape(-1, 1)
+    y = crabs[[target]].values.reshape(-1, 1).astype(int)
+
+    regModeleq = st.checkbox("Display regression model")
+    if regModeleq: show_eq(X,y,target)
+
+
+with col2:
+    fig, ax = plt.subplots()
+
+    sns.scatterplot(x=X, y=y)
+
+    ax.set_xlabel("Latitude", fontsize=14)
+    ax.set_ylabel(target, fontsize=14)
+
+    st.pyplot(fig)
