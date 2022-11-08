eaddb934fa6163a233841dad60d0031cab295dd2
cjschan <cjschan@gmail.com>
2022-11-07 21:16:31 -0600

    Update linear_regression.py

--- linear_regression.py
+++ linear_regression.py
@@ -121,12 +121,18 @@ with tab1:
         if add_reg: st.latex("\widehat{\\text{" + target + "}} = " + str(m) + "(\\text{Latitude})" + str(b))
         if add_mean: st.latex("\overline{\\text{" + target + "}} = " + str(np.round(crabs[target].mean(),2)))
         st.pyplot(fig)
-        desc1 = "Description: Samples of fiddler crabs from 13 locations were taken and the " + target
-        desc2 = " of samples from each location was recorded. As the latitude increases, the " + target
+        desc1 = "Description: Samples of fiddler crabs from 13 locations were taken and the " + thisdict[target]
+        desc2 = " from each location was recorded. As the latitude increases, the " + target
         desc3 = " of the crabs from each location increases as well."
         description = desc1 + desc2 + desc3
         st.write(description)
 
+        thisdict = {
+          "Mean length": "mean fiddler crab length",
+          "Min length": "minimum fiddler crab length",
+          "Max length": "maximum fiddler crab length",
+          "Median length": "median fiddler crab length"
+        }
         # if regModeleq:
         #     st.latex("\widehat{\\text{" + target + "}} = " + str(m) + "(\\text{Latitude})" + str(b))
 
