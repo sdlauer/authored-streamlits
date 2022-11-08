67e7f210762d61bf822c29f3bebcce104ec666d0
cjschan <cjschan@gmail.com>
2022-11-08 07:24:29 -0600

    Update linear_regression.py

--- linear_regression.py
+++ linear_regression.py
@@ -165,5 +165,5 @@ with tab4:
     st.write(ss_desc)
     st.subheader("Correlation coefficient")
     corr = np.round(np.corrcoef(crabs["Latitude"], crabs[target])[0,1],2)
-    st.write("The correlation coefficient between latitude and " + thisdict[target] + " is" + str(corr) + ", which implies a strong positive correlation. ")
-    st.write("The coefficient of determination is " + str(corr**2) + ", which means that " + str(corr*100) + "% of the variance in " + thisdict[target] + "can be explained by the variation in latitude using the least squares regression line.")
+    st.write("The correlation coefficient between latitude and " + thisdict[target] + " is " + str(corr) + ", which implies a strong positive correlation. ")
+    st.write("The coefficient of determination is " + str(corr**2) + ", which means that " + str(corr**2*100) + "% of the variance in " + thisdict[target] + "can be explained by the variation in latitude using the least squares regression line.")
