adec0ce0aa0e58ceb3b8e54c9adc26ae52abe12e
cjschan <cjschan@gmail.com>
2022-11-08 13:48:07 -0600

    Update linear_regression.py

--- linear_regression.py
+++ linear_regression.py
@@ -156,7 +156,7 @@ with tab4:
     st.subheader("Summary statistics")
     st.table(crabs[["Latitude",target]].describe().T)
     st.subheader("Sum of squared errors")
-    yPredicted = linearModel.predict(X)
+    yPredicted = linearModel.predict(crabs["Latitude"])
     SSEreg = np.round(sum((y - yPredicted)**2)[0],2)
     SSEyBar = np.round(sum((y - np.mean(crabs[target]))**2)[0],2)
     ss_desc1 = "The sum of squared errors for the mean of the " + thisdict[target] + " is " + str(SSEyBar) + ". "
