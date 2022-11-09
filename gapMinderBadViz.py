599a0a9caf9934a856a191268bf7ddc0e1c4b4c4
Matt Rissler <mrissler@wiley.com>
2022-11-09 14:49:56 -0600

    Layout?

---
+++ gapMinderBadViz.py
@@ -0,0 +1,68 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov 4 09:50:17 2022
+
+@author: mrissler
+"""
+
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+import plotly.figure_factory as ff
+
+@st.cache
+@st.cache
+def loadData():
+    df = px.data.gapminder().query("year == 2007")
+    return df
+
+
+hide = """
+        <style>
+        #MainMenu {visibility: hidden;}
+        footer {visibility: hidden;}
+        header {visibility: hidden;}
+        body {overflow: hidden;}
+        div.block-container {padding-top:1rem;}
+        div.block-container {padding-bottom:1rem;}
+        </style>
+        """
+
+st.markdown(hide, unsafe_allow_html=True)
+
+gm2007 = loadData()
+
+tab1, tab2 = st.tabs(["Recommendations", "Playground"])
+
+with tab1:
+    col1, col2 = st.columns([1,4])
+
+    with col1:
+        plotType = st.selectbox('Type of color scale',
+                                ('Rainbow',
+                                 'Yellow-Blue',
+                                 'Contrast based',
+                                 'Shape based'))
+
+        textDesc = st.checkbox('Text descriptions of plot')
+
+    with col2:
+
+        match plotType:
+            case 'Rainbow':
+                fig = px.scatter(df, x = 'gdpPercap', y = 'lifeExp',
+                            color = 'continent', size = 'logPop',
+                            labels = {'gdpPercap' : 'GDP per capita ($/person)',
+                                      'lifeExp' : 'Life expectancy (years)',
+                                      'continent' : 'Continent',
+                                      'logPop': 'log(Population)'},
+                            color_discrete_sequence = px.colors.qualitative.Set1,
+                    )
+                fig.update_layout(font_size = 12,
+                                  legend=dict(yanchor="top", y=0.7, x=.75)
+                                  )
+                st.plotly_chart(fig, use_container_width=True)
+
+            case _:
+                st.text("Not implemented yet.")
+    st.text("Recommendations for this color scale")
