b56f319f99f9a43ce0a8b25ac4d224c11cfbf621
cjschan <cjschan@gmail.com>
2022-11-09 08:22:45 -0600

    Update linear_regression.py

--- linear_regression.py
+++ linear_regression.py
@@ -24,44 +24,20 @@ crabs = pd.read_csv("crab-groups.csv")
 
 crabs.columns = ["Site", "Latitude", "Sample size", "Mean length", "Min length", "Max length", "Stdev length","Median length","Date"]
 crabs = crabs[["Site", "Date", "Sample size","Latitude","Mean length", "Min length", "Max length","Median length"]]
-# # Show linear regression equation
-# def show_eq(X,y,target):
-#     m, b = np.polyfit(X, y, 1)
-#     m = round(m,3)
-#     b = round(b,3)
-#     st.latex('\hat{\text{' + target + '}} = ' + str(m) + '(\text{Latitude}) + ' + str(b))
-#
-# # Plot linear regression line
-# def add_reg(data):
-#     m, b = np.polyfit(data[0], data[1], 1)
-#     x = [1,100]
-#     y = [m*x[0]+b, m*x[1]+b]
-#     plt.plot(x,y, c='red')
-#
-# # Add residuals to the plot
-# def add_resid(data):
-#     n = len(data[0])
-#     m, b = np.polyfit(data[0], data[1], 1)
-#     y_pred = m*data[0] + b
-#     for i in range(n):
-#         plt.plot([data[0][i],data[0][i]],[data[1][i],y_pred[i]], c = 'black')
-#
-# # Show linear regression equation
-# def show_eq(data):
-#     m, b = np.polyfit(data[0], data[1], 1)
-#     m = round(m,3)
-#     b = round(b,3)
-#     plt.text(110,0,'y =' + str(m) + 'x + ' + str(b))
-#
-# # Show correlation coefficient
-# def show_corr(data):
-#     # Note: np.corrcoef gives a correlation matrix
-#     corr_coef = np.corrcoef(data[0],data[1])
-#     corr_coef = round(corr_coef[0,1],3)
-#     plt.text(110,2,'r = ' + str(corr_coef))
-
 
 tab1, tab2, tab3, tab4 = st.tabs(["Plot", "Data","Prediction", "Summary statistics"])
+
+# Store relevant columns as variables
+X = crabs[['Latitude']]
+y = crabs[[target]]
+
+# Logistic regression predicting diagnosis from tumor radius
+linearModel = LinearRegression()
+linearModel.fit(np.ravel(X.astype(float)),np.ravel(y.astype(float)))
+
+m, b = np.polyfit(np.ravel(X).astype(float), np.ravel(y).astype(float), 1)
+m = np.round(m,3)
+b = np.round(b,3)
 
 with tab1:
     col1, col2 = st.columns([1.5,3])
@@ -77,48 +53,26 @@ with tab1:
             ]
         )
 
-        # Store relevant columns as variables
-        X = crabs[['Latitude']].values.reshape(-1, 1).astype(float)
-        y = crabs[[target]].values.reshape(-1, 1).astype(float)
-
-        # Logistic regression predicting diagnosis from tumor radius
-        linearModel = LinearRegression()
-        linearModel.fit(X,np.ravel(y.astype(int)))
-
-        # regModeleq = st.checkbox("Display regression equation")
         add_reg = st.checkbox("Add regression line")
         add_resid = st.checkbox("Add residuals", disabled=(not add_reg))
         add_mean = st.checkbox("Add mean")
 
-        m, b = np.polyfit(np.ravel(X).astype(float), np.ravel(y).astype(float), 1)
-        m = np.round(m,3)
-        b = np.round(b,3)
-
-
     with col2:
-
-
         fig, ax = plt.subplots()
-        sns.scatterplot(x="Latitude", y=target, data=crabs)
+        sns.regplot(x="Latitude", y=target, data=crabs, ci=None, fit_reg=add_reg, label="Regression line")
         ax.set_xlabel("Latitude", fontsize=14)
         ax.set_ylabel(target, fontsize=14)
-
-        if add_reg:
-            x_ind = [X.min(),X.max()]
-            y_ind = [m*x_ind[0]+b, m*x_ind[1]+b]
-            plt.plot(x_ind,y_ind, c='red', label="Regression line")
-            plt.legend()
 
         if add_mean:
             x_ind = [X.min(),X.max()]
             y_mean = [crabs[target].mean(), crabs[target].mean()]
             plt.plot(x_ind,y_mean, c='darkorange', label="Mean")
-            plt.legend()
 
         if add_resid:
             n = len(X)
             for i in range(len(X)):
                 plt.plot([X[i],X[i]],[y[i],m*X[i]+b],color='grey',linewidth = 2)
+        plt.legend()
         st.subheader("Plot")
 
         st.pyplot(fig)
@@ -156,8 +110,7 @@ with tab4:
     st.subheader("Summary statistics")
     st.table(crabs[["Latitude",target]].describe().T)
     st.subheader("Sum of squared errors")
-    yPredicted = linearModel.predict(X)
-    SSEreg = sum((y - yPredicted)**2)[0]
+    SSEreg = sum((y - (m*X + b))**2)
     SSEyBar = np.round(sum((y - np.mean(crabs[target]))**2)[0],2)
     ss_desc1 = "The sum of squared errors for the mean of the " + thisdict[target] + " is " + str(SSEyBar) + ". "
     ss_desc2 = "The sum of squared errors for the least squares regression line is " + str(SSEreg) + ". "
