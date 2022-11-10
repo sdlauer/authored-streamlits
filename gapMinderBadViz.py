5874c23fe277fa447c81a3b028b0f9c94533310d
Matt Rissler <mrissler@wiley.com>
2022-11-09 21:23:59 -0600

    indents

--- gapMinderBadViz.py
+++ gapMinderBadViz.py
@@ -35,161 +35,161 @@ st.markdown(hide, unsafe_allow_html=True)
 
 gm2007 = loadData()
 
-    col1, col2 = st.columns([1,3])
+col1, col2 = st.columns([1,3])
 
-    baseTextDesc = '''The horizontal axis is labeled "GDP per capita (\$/person)" and ranges from 0 to 50k.
-    The vertical axis is labeled "Life expectancy (years)" and ranges from 39 to 83.
-    Most points are follow a curve where low income (GDP per capita) countries (below \$2000) have low life expectancy (below 60 years),
-    but life expectancy increases rapidly with income with countries with an average income above \$5000 have a life expectancy above 70 years. '''
+baseTextDesc = '''The horizontal axis is labeled "GDP per capita (\$/person)" and ranges from 0 to 50k.
+The vertical axis is labeled "Life expectancy (years)" and ranges from 39 to 83.
+Most points are follow a curve where low income (GDP per capita) countries (below \$2000) have low life expectancy (below 60 years),
+but life expectancy increases rapidly with income with countries with an average income above \$5000 have a life expectancy above 70 years. '''
 
-    continentDesc = ''' Most low income/low life expectancy countries are from Africa.
-    European countries dominate the countries with high life expectancy.
-    Countries in Asia and the Americas make up many of the countries with relatively low income and high life expectancy.'''
+continentDesc = ''' Most low income/low life expectancy countries are from Africa.
+European countries dominate the countries with high life expectancy.
+Countries in Asia and the Americas make up many of the countries with relatively low income and high life expectancy.'''
 
-    with col1:
-        plotType = st.selectbox('Type of color scale',
-                                ('Rainbow',
-                                 'Yellow-Blue-Green',
-                                 'Contrast based',
-                                 'Shape based'))
+with col1:
+    plotType = st.selectbox('Type of color scale',
+                            ('Rainbow',
+                             'Yellow-Blue-Green',
+                             'Contrast based',
+                             'Shape based'))
 
-        textDesc = st.checkbox('Text description of plot')
+    textDesc = st.checkbox('Text description of plot')
 
-    with col2:
+with col2:
 
-        if plotType == "Rainbow":
-            if textDesc:
-                st.markdown(baseTextDesc+'''The continents for this plot are colored red for Asia, blue for Europe,
-                green for Africa, purple for the Americas, and orange for Oceania.'''+continentDesc)
+    if plotType == "Rainbow":
+        if textDesc:
+            st.markdown(baseTextDesc+'''The continents for this plot are colored red for Asia, blue for Europe,
+            green for Africa, purple for the Americas, and orange for Oceania.'''+continentDesc)
 
-
-            else:
-                fig = px.scatter(gm2007, x = 'gdpPercap', y = 'lifeExp',
-                            color = 'continent', size = 'logPop',
-                            labels = {'gdpPercap' : 'GDP per capita ($/person)',
-                                      'lifeExp' : 'Life expectancy (years)',
-                                      'continent' : 'Continent',
-                                      'logPop': 'log(Population)',
-                                      'pop':'Population'},
-                            hover_data={'gdpPercap':':.2f',
-                                        'lifeExp':':.1f',
-                                        'logPop':False,
-                                        'pop':True},
-                            color_discrete_sequence = px.colors.qualitative.Set1,
-                    )
-                fig.update_layout(font_size = 12,
-                                  legend=dict(yanchor="top", y=0.7, x=.75)
-                                  )
-                st.plotly_chart(fig, use_container_width=True)
-
-        elif plotType == "Yellow-Blue-Green":
-            if textDesc:
-                st.markdown(baseTextDesc+'''The continents for this plot are colored light yellow for Asia, yellow for Europe,
-                yellow-green for Africa, green for the Americas, and blue for Oceania.
-                The yellow colors are hard to see against the grey background.'''+continentDesc)
-
-
-            else:
-                fig = px.scatter(gm2007, x = 'gdpPercap', y = 'lifeExp',
-                            color = 'continent', size = 'logPop',
-                            labels = {'gdpPercap' : 'GDP per capita ($/person)',
-                                      'lifeExp' : 'Life expectancy (years)',
-                                      'continent' : 'Continent',
-                                      'logPop': 'log(Population)',
-                                      'pop':'Population'},
-                            hover_data={'gdpPercap':':.2f',
-                                        'lifeExp':':.1f',
-                                        'logPop':False,
-                                        'pop':True},
-                            color_discrete_sequence = px.colors.colorbrewer.YlGnBu,
-                    )
-                fig.update_layout(font_size = 12,
-                                  legend=dict(yanchor="top", y=0.7, x=.75)
-                                  )
-                st.plotly_chart(fig, use_container_width=True)
-
-        elif plotType == "Contrast based":
-            if textDesc:
-                st.markdown(baseTextDesc+'''The continents for this plot are
-                colored almost white for Asia, light grey for Europe,
-                grey for Africa, grey blue for the Americas, and blue for Oceania.
-                The light colors are hard to see against the grey background.'''+continentDesc)
-
-
-            else:
-                fig = px.scatter(gm2007, x = 'gdpPercap', y = 'lifeExp',
-                            color = 'continent', size = 'logPop',
-                            labels = {'gdpPercap' : 'GDP per capita ($/person)',
-                                      'lifeExp' : 'Life expectancy (years)',
-                                      'continent' : 'Continent',
-                                      'logPop': 'log(Population)',
-                                      'pop':'Population'},
-                            hover_data={'gdpPercap':':.2f',
-                                        'lifeExp':':.1f',
-                                        'logPop':False,
-                                        'pop':True},
-                            color_discrete_sequence = px.colors.colorbrewer.Blues,
-                    )
-                fig.update_layout(font_size = 12,
-                                  legend=dict(yanchor="top", y=0.7, x=.75)
-                                  )
-                st.plotly_chart(fig, use_container_width=True)
-
-        elif plotType == "Shape based":
-            if textDesc:
-                st.markdown(baseTextDesc+'''The continents for this plot are
-                shown with green circles for Asia, orange diamonds for Europe,
-                purple squares for Africa, pink x for the Americas, and green +
-                for Oceania.'''+continentDesc)
-
-
-            else:
-                fig = px.scatter(gm2007, x = 'gdpPercap', y = 'lifeExp',
-                            color = 'continent', size = 'logPop',
-                            symbol = 'continent',
-                            labels = {'gdpPercap' : 'GDP per capita ($/person)',
-                                      'lifeExp' : 'Life expectancy (years)',
-                                      'continent' : 'Continent',
-                                      'logPop': 'log(Population)',
-                                      'pop':'Population'},
-                            hover_data={'gdpPercap':':.2f',
-                                        'lifeExp':':.1f',
-                                        'logPop':False,
-                                        'pop':True},
-                            color_discrete_sequence = px.colors.colorbrewer.Dark2,
-                    )
-                fig.update_layout(font_size = 12,
-                                  legend=dict(yanchor="top", y=0.7, x=.75)
-                                  )
-                st.plotly_chart(fig, use_container_width=True)
 
         else:
-                st.text("Not implemented yet.")
-
-    st.header("Recommendation")
-    if plotType == "Rainbow":
-        st.markdown('''Do **not** use a rainbow scale. The contrast between colors in rainbow scales
-                   are not uniformly spaced making distinguishing colors difficult for people
-                   with CVD.''')
+            fig = px.scatter(gm2007, x = 'gdpPercap', y = 'lifeExp',
+                        color = 'continent', size = 'logPop',
+                        labels = {'gdpPercap' : 'GDP per capita ($/person)',
+                                  'lifeExp' : 'Life expectancy (years)',
+                                  'continent' : 'Continent',
+                                  'logPop': 'log(Population)',
+                                  'pop':'Population'},
+                        hover_data={'gdpPercap':':.2f',
+                                    'lifeExp':':.1f',
+                                    'logPop':False,
+                                    'pop':True},
+                        color_discrete_sequence = px.colors.qualitative.Set1,
+                )
+            fig.update_layout(font_size = 12,
+                              legend=dict(yanchor="top", y=0.7, x=.75)
+                              )
+            st.plotly_chart(fig, use_container_width=True)
 
     elif plotType == "Yellow-Blue-Green":
-        st.markdown('''Use a yellow-blue or green-blue color scale.
-        By avoiding red, people with CVD are better
-        able to distinguish colors.''')
+        if textDesc:
+            st.markdown(baseTextDesc+'''The continents for this plot are colored light yellow for Asia, yellow for Europe,
+            yellow-green for Africa, green for the Americas, and blue for Oceania.
+            The yellow colors are hard to see against the grey background.'''+continentDesc)
+
+
+        else:
+            fig = px.scatter(gm2007, x = 'gdpPercap', y = 'lifeExp',
+                        color = 'continent', size = 'logPop',
+                        labels = {'gdpPercap' : 'GDP per capita ($/person)',
+                                  'lifeExp' : 'Life expectancy (years)',
+                                  'continent' : 'Continent',
+                                  'logPop': 'log(Population)',
+                                  'pop':'Population'},
+                        hover_data={'gdpPercap':':.2f',
+                                    'lifeExp':':.1f',
+                                    'logPop':False,
+                                    'pop':True},
+                        color_discrete_sequence = px.colors.colorbrewer.YlGnBu,
+                )
+            fig.update_layout(font_size = 12,
+                              legend=dict(yanchor="top", y=0.7, x=.75)
+                              )
+            st.plotly_chart(fig, use_container_width=True)
 
     elif plotType == "Contrast based":
+        if textDesc:
+            st.markdown(baseTextDesc+'''The continents for this plot are
+            colored almost white for Asia, light grey for Europe,
+            grey for Africa, grey blue for the Americas, and blue for Oceania.
+            The light colors are hard to see against the grey background.'''+continentDesc)
 
-        st.markdown('''Use a scale that depends on changes in contrast.
-        By only changing the contrast, people with CVD can distinguish the
-        colors as well as other people. But, some shades lack contrast with
-        the background, emphasizing those with high contrast compared to the
-        background.''')
+
+        else:
+            fig = px.scatter(gm2007, x = 'gdpPercap', y = 'lifeExp',
+                        color = 'continent', size = 'logPop',
+                        labels = {'gdpPercap' : 'GDP per capita ($/person)',
+                                  'lifeExp' : 'Life expectancy (years)',
+                                  'continent' : 'Continent',
+                                  'logPop': 'log(Population)',
+                                  'pop':'Population'},
+                        hover_data={'gdpPercap':':.2f',
+                                    'lifeExp':':.1f',
+                                    'logPop':False,
+                                    'pop':True},
+                        color_discrete_sequence = px.colors.colorbrewer.Blues,
+                )
+            fig.update_layout(font_size = 12,
+                              legend=dict(yanchor="top", y=0.7, x=.75)
+                              )
+            st.plotly_chart(fig, use_container_width=True)
 
     elif plotType == "Shape based":
+        if textDesc:
+            st.markdown(baseTextDesc+'''The continents for this plot are
+            shown with green circles for Asia, orange diamonds for Europe,
+            purple squares for Africa, pink x for the Americas, and green +
+            for Oceania.'''+continentDesc)
 
-        st.markdown('''Use changes in another feature (size or shape).
-        Users can depend on this other feature if the color scale is
-        difficult to perceive.''')
+
+        else:
+            fig = px.scatter(gm2007, x = 'gdpPercap', y = 'lifeExp',
+                        color = 'continent', size = 'logPop',
+                        symbol = 'continent',
+                        labels = {'gdpPercap' : 'GDP per capita ($/person)',
+                                  'lifeExp' : 'Life expectancy (years)',
+                                  'continent' : 'Continent',
+                                  'logPop': 'log(Population)',
+                                  'pop':'Population'},
+                        hover_data={'gdpPercap':':.2f',
+                                    'lifeExp':':.1f',
+                                    'logPop':False,
+                                    'pop':True},
+                        color_discrete_sequence = px.colors.colorbrewer.Dark2,
+                )
+            fig.update_layout(font_size = 12,
+                              legend=dict(yanchor="top", y=0.7, x=.75)
+                              )
+            st.plotly_chart(fig, use_container_width=True)
 
     else:
-        st.text("Recommendations for this color scale not yet implemented")
+            st.text("Not implemented yet.")
+
+st.header("Recommendation")
+if plotType == "Rainbow":
+    st.markdown('''Do **not** use a rainbow scale. The contrast between colors in rainbow scales
+               are not uniformly spaced making distinguishing colors difficult for people
+               with CVD.''')
+
+elif plotType == "Yellow-Blue-Green":
+    st.markdown('''Use a yellow-blue or green-blue color scale.
+    By avoiding red, people with CVD are better
+    able to distinguish colors.''')
+
+elif plotType == "Contrast based":
+
+    st.markdown('''Use a scale that depends on changes in contrast.
+    By only changing the contrast, people with CVD can distinguish the
+    colors as well as other people. But, some shades lack contrast with
+    the background, emphasizing those with high contrast compared to the
+    background.''')
+
+elif plotType == "Shape based":
+
+    st.markdown('''Use changes in another feature (size or shape).
+    Users can depend on this other feature if the color scale is
+    difficult to perceive.''')
+
+else:
+    st.text("Recommendations for this color scale not yet implemented")
