1b4f5f17002f4c6b7c55b9298de8933a7e533984
Aimee Schwab-McCoy <39537992+aimeeschwab-mccoy@users.noreply.github.com>
2022-11-16 09:46:53 -0600

    Add files via upload
    Caching data and images

--- streamlit_ch10_sec7_travel.py
+++ streamlit_ch10_sec7_travel.py
@@ -19,10 +19,15 @@ hide = """
 
 st.markdown(hide, unsafe_allow_html=True)
 
-url = "https://raw.githubusercontent.com/aimeeschwab-mccoy/streamlit_asm/main/tripadvisor_review.csv"
+@st.cache
+def loadData():
+    url = "https://raw.githubusercontent.com/aimeeschwab-mccoy/streamlit_asm/main/tripadvisor_review.csv"
 
-reviews = pd.read_csv(url)
-reviews.columns = list(reviews.columns)
+    reviews = pd.read_csv(url)
+    reviews.columns = list(reviews.columns)
+    return reviews
+
+reviews = loadData()
 
 X = reviews.drop(axis=1, labels=['User ID'])
 kmModel = KMeans(n_clusters = 4, random_state=123)
@@ -30,7 +35,11 @@ kmModel = kmModel.fit(X)
 clusters = kmModel.fit_predict(X)
 centroids = kmModel.cluster_centers_
 
-
+images = {"Art": "clustering_images/art.png", "Clubs": "clustering_images/clubs.png", 
+            "Juice bars": "clustering_images/juice.png", "Restaurants": "clustering_images/restaurants.png", 
+            "Museums": "clustering_images/museums.png", "Resorts": "clustering_images/resorts.png",
+            "Parks": "clustering_images/parks.png", "Beaches": "clustering_images/beaches.png", 
+            "Theaters": "clustering_images/theaters.png", "Religious sites": "clustering_images/religious.png"}
 
 col1, col2 = st.columns([2,3])
 
@@ -40,7 +49,7 @@ with col1:
         "Select destination type",
         [
             "Art", "Clubs", "Juice bars", "Restaurants", "Museums", "Resorts",
-            "Parks", "Beaches", "Theaters", "Religious"
+            "Parks", "Beaches", "Theaters", "Religious sites"
         ]
     )
 
@@ -51,12 +60,13 @@ with col1:
         st.dataframe(summary)
 
 with col2:
-    fig, ax = plt.subplots()
+    #fig, ax = plt.subplots()
 
-    p = sns.kdeplot(data=X, x=destination, hue=clusters, palette="viridis", linewidth=2.5)
-    p.set_ylabel("Density", fontsize=14)
-    p.set_xlabel(destination, fontsize=14)
-    p.set_title("Travel ratings by cluster", fontsize=16)
+    #p = sns.kdeplot(data=X, x=destination, hue=clusters, palette="viridis", linewidth=2.5)
+    #p.set_ylabel("Density", fontsize=14)
+    #p.set_xlabel(destination, fontsize=14)
+    #p.set_title("Travel ratings by cluster", fontsize=16)
 
+    #st.pyplot(fig)
 
-    st.pyplot(fig)
+    st.image(images[destination])
