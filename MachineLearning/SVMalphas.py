967e247f42d04912c332cfeec7269502448a1c8f
Matt Rissler <mrissler@wiley.com>
2023-01-27 08:44:52 -0600

    All the numbers

--- MachineLearning/SVMalphas.py
+++ MachineLearning/SVMalphas.py
@@ -56,24 +56,24 @@ with col1:
     alphas = np.zeros(20)
     #st.write("$\\alpha_1$")
 
-    alphas[1] = st.slider(label=''':orange[$ \\alpha_1 $ : Choose a value between 0 and 3.]''', min_value=0.0, 
+    alphas[1] = st.slider(label=''':orange[$ \\alpha_1 $]: Choose a value between 0 and 3.''', min_value=0.0, 
                           max_value=3.0, value=0.5, step=0.01,key = 'alpha1')
 
     #st.write("$\\alpha_2$")
 
-    alphas[2] = st.slider(label="$\\alpha_2$: Choose a value between 0 and 3.", min_value=0.0, max_value=3.0, value=0.5, step=0.01)
+    alphas[2] = st.slider(label=":orange[$\\alpha_2$]: Choose a value between 0 and 3.", min_value=0.0, max_value=3.0, value=0.5, step=0.01)
 
     #st.write("$\\alpha_6$")
 
-    alphas[6] = st.slider(label=":blue[$\\alpha_6$: Choose a value between 0 and 3.]", min_value=0.0, max_value=3.0, value=0.33, step=0.01)
+    alphas[6] = st.slider(label=":blue[$\\alpha_6$]: Choose a value between 0 and 3.", min_value=0.0, max_value=3.0, value=0.33, step=0.01)
 
     #st.write("$\\alpha_{11}$")
 
-    alphas[11] = st.slider(label="$\\alpha_{11}$: Choose a value between 0 and 3.", min_value=0.0, max_value=3.0, value=0.34, step=0.01)
+    alphas[11] = st.slider(label=":blue[$\\alpha_{11}$]: Choose a value between 0 and 3.", min_value=0.0, max_value=3.0, value=0.34, step=0.01)
     
     #st.write("$\\alpha_{13}$")
 
-    alphas[13] = st.slider(label="$\\alpha_{13}$: Choose a value between 0 and 3.", min_value=0.0, max_value=3.0, value=0.33, step=0.01)
+    alphas[13] = st.slider(label=":blue[$\\alpha_{13}$]: Choose a value between 0 and 3.", min_value=0.0, max_value=3.0, value=0.33, step=0.01)
 
     weights = np.multiply(np.array(alphas),np.array(y2))*np.matrix(X)
 
