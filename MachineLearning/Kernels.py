e04f0b8b52a3075f4658dd61ce36d1a8e8e94165
Matt Rissler <mrissler@wiley.com>
2023-02-10 13:48:02 -0600

    colon

--- MachineLearning/Kernels.py
+++ MachineLearning/Kernels.py
@@ -45,7 +45,7 @@ with col1:
     gamma = 1
     if kernel == 'poly':
         degree = st.slider('Degree:', min_value = 1, max_value = 7)
-    else
+    else:
         gamma = st.slider('gamma:', min_value=0.1, max_value=10.0)
 
 pts = np.array(4*np.random.rand(num_pts,2)-2)
