ec0d07c219fd2d8ac948ee8d505734d50c2808bb
Matt Rissler <mrissler@wiley.com>
2023-02-13 12:51:15 -0600

    no map

--- MachineLearning/Kernels.py
+++ MachineLearning/Kernels.py
@@ -83,9 +83,9 @@ with col2:
         else:
             log_levels = []
         log_levels = np.append(log_levels, 10**np.arange(1, np.log10(np.max(ZZ))))
-        CS = ax.contour(XX, YY, ZZ, levels=log_levels, cmap=Colormap('Greys'))
+        CS = ax.contour(XX, YY, ZZ, levels=log_levels, cmap='Greys')
     else:
-        CS = ax.contour(XX, YY, ZZ, cmap=Colormap('Greys'))
+        CS = ax.contour(XX, YY, ZZ, cmap='Greys')
     ax.clabel(CS, inline=True, fontsize=10)
     if kernel == 'poly':
         ax.set_title('Polynomial with degree = '+ str(degree))
