7bcd2f907d08d9f672a0126e6a99c60454a71778
Matt Rissler <mrissler@wiley.com>
2023-02-14 09:33:44 -0600

    divisions and points

--- MachineLearning/Kernels.py
+++ MachineLearning/Kernels.py
@@ -55,6 +55,8 @@ with col1:
     new_pts = st.button('New points')
 
     tabularView = st.checkbox('Tabular view')
+    if tabularView:
+        numDivisions = st.slider('Number of values in each direction', min_value=7, max_value = 25, step = 6)
 
 if (st.session_state['old_num_pts'] != num_pts or new_pts):
     st.session_state.pts = np.array(4*np.random.rand(num_pts,2)-2)
@@ -104,8 +106,9 @@ with col2:
         st.pyplot(fig)
     
     else:
-        XX = np.linspace(-3,3,13)
-        YY = np.linspace(-3,3,13)
+        st.write(pts)
+        XX = np.linspace(-3,3,numDivisions)
+        YY = np.linspace(-3,3,numDivisions)
         ZZ = np.zeros([ len(YY), len(XX)])
         i=0
         for xx in XX:
@@ -120,5 +123,6 @@ with col2:
         ZZ = np.round(ZZ, 2)
         ZZ = pd.DataFrame(ZZ, index = XX)
         ZZ.columns = YY
+        st.write('''$x \\rightarrow$''')
         st.write(ZZ)
     #st.text(log_levels)
